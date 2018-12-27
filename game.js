2662e7d847675a181b0e8a4a9e3473617a4194c0
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-12-26 21:16:58 -0800

    added animation for clicks and divs

--- game.js
+++ game.js
@@ -1,10 +1,25 @@
 // global varialbes and arrays
 var buttonColors = ["red", "blue", "green", "yellow"];
-
-// pattern
+// game pattern
 var gamePattern = [];
+// user pattern
+var userClickedPattern = [];
 
 //functions and methods for simon game
+
+
+$(".btn").click(function() {
+
+  var userChosenColor = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColor);
+
+  console.log(userClickedPattern);
+
+});
+
+
+//next sequence
 function nextSequence(){
   // generate random number
   var randomNumber = Math.floor(Math.random() * 4);
@@ -13,9 +28,22 @@ function nextSequence(){
   //push to gamePattern
   gamePattern.push(randomChosenColor);
   // select the button with same color as randomChosenColour then create flash
-  $("#" + randomChosenColor).fadeIn().fadeOut().fadeIn();
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
   //play audio for chosen color
-  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  playSound(randomChosenColor);
+}
 
-  console.log(randomNumber);
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor){
+  //pressed animation
+  $("#" + currentColor).addClass("pressed");
+
+  // remove class animation
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed");
+  },100);
 }
