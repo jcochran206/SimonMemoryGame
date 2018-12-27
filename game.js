5b48950fba2c43ce1085445f59ee55ad5abdace2
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-12-26 16:26:27 -0800

    added gamepattern, random number generator, and assigning colors to array to push to sequence

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-alert("hello");
+// global varialbes and arrays
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+// pattern
+var gamePattern = [];
+
+//functions and methods for simon game
+function nextSequence(){
+  // generate random number
+  var randomNumber = Math.floor(Math.random() * 4);
+  // generate random color picker
+  var randomChosenColor = buttonColors[randomNumber];
+  //push to gamePattern
+  gamePattern.push(randomChosenColor);
+  // select the button with same color as randomChosenColour then create flash
+  $("#" + randomChosenColor).fadeIn().fadeOut().fadeIn();
+  //play audio for chosen color
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+
+  console.log(randomNumber);
+}
